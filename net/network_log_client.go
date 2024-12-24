f7437312f06c0075eb3205b6aac84ca53f08433f
Longoria Yu <longoria.yu@thecarousell.com>
2024-12-24 12:45:32 +0800

    Basic networking with UDP

--- net/network_log_client.go
+++ net/network_log_client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"time"
 )
 
 // Need to set up the log server first
@@ -10,16 +11,21 @@ import (
 // $ nc -lk 1902
 // -l: listen mode
 // -k: Forces nc to stay listening for another connection after its current connection is completed.
+// -u: Use UDP instead of the default option of TCP.
 func main() {
-	conn, err := net.Dial("tcp", "localhost:1902")
+	// conn, err := net.Dial("tcp", "localhost:1902")
+
+	// use UDP to connect to the server for addressing the back-pressure problem
+	timeout := 5 * time.Second
+	conn, err := net.DialTimeout("udp", "localhost:1902", timeout)
 	if err != nil {
-		log.Fatal(err)
+		log.Panicln(err)
 	}
 	defer conn.Close()
 
 	log.Println("Connected to the server")
 
-	f := log.Ldate | log.Ltime | log.Lshortfile
+	f := log.Ldate | log.Ltime | log.Lmicroseconds | log.LUTC | log.Lshortfile
 	logger := log.New(conn, "example: ", f)
 	logger.Println("This is a regular message.")
 	logger.Panicln("This is a panic.")
